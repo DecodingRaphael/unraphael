5bb5f4a30266ead7bb8b1d26aebb09c6b2b1a669
Stef Smeets <s.smeets@esciencecenter.nl>
2024-04-02 14:45:22 +0200

    Load config from yaml

--- dash/dashboard.py
+++ dash/dashboard.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from pathlib import Path
 
 import streamlit as st
+import yaml
 from scipy.cluster.hierarchy import linkage
 from seaborn import clustermap
 from sidebar_logo import add_sidebar_logo
@@ -16,6 +17,13 @@ from unraphael.feature import (
 )
 from unraphael.io import load_images_from_drc
 
+st.set_page_config(
+    page_title='Unraphael dashboard', page_icon=':framed-picture:', layout='wide'
+)
+st.write(
+    '<style>textarea[class^="st-"] { font-family: monospace; font-size: 14px; }</style>',
+    unsafe_allow_html=True,
+)
 add_sidebar_logo()
 
 st.title('Input images')
@@ -24,7 +32,12 @@ with st.sidebar:
     image_drc = st.text_input(label='Image directory', value='../data/raw/Bridgewater')
     image_drc = Path(image_drc)
 
-    width = st.number_input('Width', value=540, step=10)
+    config_fn = st.text_input(label='Config file', value='config.yaml')
+    config_fn = Path(config_fn)
+
+    width = st.number_input('Width', value=50, step=10)
+
+    # method = st.number_input('Width', value=50, step=10)
 
 if not image_drc.exists():
     st.error(f'Cannot find {image_drc}.')
@@ -32,6 +45,97 @@ if not image_drc.exists():
 images = load_images_from_drc(image_drc, width=width)
 
 show_images(images, n_cols=4)
+
+
+def _dump_config(cfg: dict) -> str:
+    def cfg_str_representer(dumper, in_str):
+        if '\n' in in_str:  # use '|' style for multiline strings
+            return dumper.represent_scalar('tag:yaml.org,2002:str', in_str, style='|')
+        return dumper.represent_scalar('tag:yaml.org,2002:str', in_str)
+
+    yaml.representer.SafeRepresenter.add_representer(str, cfg_str_representer)
+    return yaml.safe_dump(cfg, sort_keys=False, allow_unicode=True)
+
+
+sift_config = {
+    'upsampling': 2,
+    'n_octaves': 8,
+    'n_scales': 3,
+    'sigma_min': 1.6,
+    'sigma_in': 0.5,
+    'c_dog': 0.013333333333333334,
+    'c_edge': 10,
+    'n_bins': 36,
+    'lambda_ori': 1.5,
+    'c_max': 0.8,
+    'lambda_descr': 6,
+    'n_hist': 4,
+    'n_ori': 8,
+}
+
+st.session_state.sift_config = _dump_config(sift_config)
+
+orb_config = {
+    'downscale': 1.2,
+    'n_scales': 8,
+    'n_keypoints': 500,
+    'fast_n': 9,
+    'fast_threshold': 0.08,
+    'harris_k': 0.04,
+}
+
+st.session_state.orb_config = _dump_config(orb_config)
+
+ransac_config = {
+    'max_trials': 100,
+    'stop_sample_num': float('inf'),
+    'stop_residuals_sum': 0,
+    'stop_probability': 1,
+    'rng': None,
+}
+
+st.session_state.ransac_config = _dump_config(ransac_config)
+
+col1, col2 = st.columns(2)
+
+method = col1.radio(
+    'Select similarity metric',
+    ['None', 'SIFT', 'ORB', 'Outline', 'Scale'],
+    captions=[
+        'Do nothing',
+        'https://scikit-image.org/docs/stable/auto_examples/features_detection/plot_sift.html',
+        'https://scikit-image.org/docs/stable/auto_examples/features_detection/plot_orb.html',
+        'Not implemented',
+        'Not implemented',
+    ],
+)
+
+if method == 'SIFT':
+    col2.text_area(
+        label='[SIFT config parameters](https://scikit-image.org/docs/stable/api/skimage.feature.html#skimage.feature.SIFT)',
+        key='sift_config',
+        height=200,
+    )
+elif method == 'ORB':
+    col2.text_area(
+        label='[ORB config parameters](https://scikit-image.org/docs/stable/api/skimage.feature.html#skimage.feature.ORB)',
+        key='sift_config',
+        height=200,
+    )
+
+if method in ('SIFT', 'ORB'):
+    col2.text_area(
+        label='[RANSAC config parameters](https://scikit-image.org/docs/stable/api/skimage.measure.html#skimage.measure.ransac)',
+        key='ransac_config',
+        height=200,
+    )
+
+if method == 'None':
+    st.stop()
+
+if method in ('Outline', 'Scale'):
+    raise NotImplementedError(method)
+    st.stop()
 
 st.title('Feature extraction')
 
