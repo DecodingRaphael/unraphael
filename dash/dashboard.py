15b2d8013cf4d2a6cfb3ac061a399ccd0e108688
Stef Smeets <s.smeets@esciencecenter.nl>
2024-04-03 11:35:11 +0200

    Refactor dashboard

--- dash/dashboard.py
+++ dash/dashboard.py
@@ -1,54 +1,18 @@
 from __future__ import annotations
 
-from pathlib import Path
-
 import streamlit as st
-import yaml
+from config import dump_config, to_session_state
 from scipy.cluster.hierarchy import linkage
 from seaborn import clustermap
 from sidebar_logo import add_sidebar_logo
 from skimage.feature import ORB, SIFT
-from widgets import show_images
+from widgets import load_config, load_images, show_images
 
 from unraphael.feature import (
     detect_and_extract,
     get_heatmaps,
     heatmap_to_condensed_distance_matrix,
 )
-from unraphael.io import load_images_from_drc
-
-
-@st.cache_data
-def _load_images_from_drc(*args, **kwargs):
-    return load_images_from_drc(*args, **kwargs)
-
-
-@st.cache_data
-def load_config(fn: Path | str):
-    fn = Path(fn)
-
-    if not fn.exists():
-        st.error(f'Cannot find {fn}.')
-
-    with open(fn) as f:
-        config = yaml.safe_load(f)
-
-    return config
-
-
-def _to_session_state(key: str, section: str | None = None):
-    section = section if section else key
-    st.session_state.config[section] = yaml.safe_load(st.session_state[key])
-
-
-def _dump_config(cfg: dict) -> str:
-    def cfg_str_representer(dumper, in_str):
-        if '\n' in in_str:  # use '|' style for multiline strings
-            return dumper.represent_scalar('tag:yaml.org,2002:str', in_str, style='|')
-        return dumper.represent_scalar('tag:yaml.org,2002:str', in_str)
-
-    yaml.representer.SafeRepresenter.add_representer(str, cfg_str_representer)
-    return yaml.safe_dump(cfg, sort_keys=False, allow_unicode=True)
 
 
 def main():
@@ -64,33 +28,8 @@ def main():
     st.title('Input images')
 
     with st.sidebar:
-        image_drc = st.text_input(label='Image directory', value='../data/raw/Bridgewater')
-        image_drc = Path(image_drc)
-
-        if not image_drc.exists():
-            st.error(f'Cannot find {image_drc}.')
-
-        config_fn = st.text_input(label='Config file', value='config.yaml')
-        config = load_config(config_fn)
-
-        if 'config' not in st.session_state:
-            st.session_state.config = config
-            st.session_state.width = st.session_state.config['width']
-            st.session_state.method = st.session_state.config['method']
-
-        st.download_button(
-            label='Download config',
-            data=_dump_config(st.session_state.config),
-            file_name='my_config.yaml',
-            mime='text/yaml',
-            disabled=True,
-        )
-
-        width = st.number_input(
-            'Width', step=10, key='width', on_change=_to_session_state, kwargs={'key': 'width'}
-        )
-
-    images = _load_images_from_drc(image_drc, width=width)
+        load_config()
+        images = load_images()
 
     show_images(images, n_cols=8)
 
@@ -103,7 +42,7 @@ def main():
         'Select similarity metric',
         options=method_options,
         key='method',
-        on_change=_to_session_state,
+        on_change=to_session_state,
         kwargs={'key': 'method'},
     )
 
@@ -111,16 +50,16 @@ def main():
 
     if method == 'sift':
         label1 = '[SIFT config parameters](https://scikit-image.org/docs/stable/api/skimage.feature.html#skimage.feature.SIFT)'
-        st.session_state.col1 = _dump_config(st.session_state.config['sift'])
+        st.session_state.col1 = dump_config(st.session_state.config['sift'])
     elif method == 'orb':
         label1 = '[ORB config parameters](https://scikit-image.org/docs/stable/api/skimage.feature.html#skimage.feature.ORB)'
-        st.session_state.col1 = _dump_config(st.session_state.config['orb'])
+        st.session_state.col1 = dump_config(st.session_state.config['orb'])
     else:
         label1 = 'nrst'
 
     if st.session_state.method in ('sift', 'orb'):
         label2 = '[RANSAC config parameters](https://scikit-image.org/docs/stable/api/skimage.measure.html#skimage.measure.ransac)'
-        st.session_state.col2 = _dump_config(st.session_state.config['ransac'])
+        st.session_state.col2 = dump_config(st.session_state.config['ransac'])
     else:
         label2 = 'heai'
 
@@ -128,14 +67,14 @@ def main():
         label=label1,
         key='col1',
         height=200,
-        on_change=_to_session_state,
+        on_change=to_session_state,
         kwargs={'key': 'col1', 'section': method},
     )
     col2.text_area(
         label=label2,
         key='col2',
         height=200,
-        on_change=_to_session_state,
+        on_change=to_session_state,
         kwargs={'key': 'col2', 'section': 'ransac'},
     )
 
--- dash/widgets.py
+++ dash/widgets.py
@@ -1,16 +1,55 @@
 from __future__ import annotations
 
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 import streamlit as st
+from config import _load_config, _update_session_state, dump_session_state, to_session_state
+
+from unraphael.io import load_images_from_drc
+
+_load_images_from_drc = st.cache_data(load_images_from_drc)
+
 
 if TYPE_CHECKING:
     import numpy as np
 
 
 def show_images(images: dict[str, np.ndarray], *, n_cols: int = 4):
+    """Widget to show images with given number of columns."""
     cols = st.columns(n_cols)
 
     for i, (name, im) in enumerate(images.items()):
         col = cols[i % n_cols]
         col.image(im, use_column_width=True, caption=name)
+
+
+def load_config():
+    """Widget to load config file."""
+    config_fn = st.text_input(label='Config file', value='config.yaml')
+    config = _load_config(config_fn)
+    if 'config' not in st.session_state:
+        _update_session_state(config)
+
+    st.download_button(
+        label='Download config',
+        data=dump_session_state(),
+        file_name='my_config.yaml',
+        mime='text/yaml',
+        disabled=False,
+    )
+
+
+def load_images():
+    """Widget to load images."""
+    image_drc = st.text_input(label='Image directory', value='../data/raw/Bridgewater')
+    image_drc = Path(image_drc)
+
+    if not image_drc.exists():
+        st.error(f'Cannot find {image_drc}.')
+
+    width = st.number_input(
+        'Width', step=10, key='width', on_change=to_session_state, kwargs={'key': 'width'}
+    )
+
+    return _load_images_from_drc(image_drc, width=width)
