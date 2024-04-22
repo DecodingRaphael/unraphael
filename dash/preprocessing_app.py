8c7deda2ccf9e03827cb62472454bfcd73a2c304
ThijsVroegh <thijs.vroegh@outlook.com>
2024-04-22 10:34:38 +0200

    added two download options for extracting the figures from the original image, in addition to two options to download processed figures

--- dash/preprocessing_app.py
+++ dash/preprocessing_app.py
@@ -57,12 +57,18 @@ div.stSlider > div[data-baseweb="slider"] > div > div {{
 # Apply color shading on slider
 color_slider = st.markdown(color_shading, unsafe_allow_html=True)
 
+# Apply mask to original image and convert it to PIL format
+def apply_mask_and_convert_to_pil(original_image, mask):
+    # Apply mask to original image
+    extracted_image = cv2.bitwise_and(original_image, original_image, mask=mask)
+    # Convert extracted image to PIL format
+    return Image.fromarray(extracted_image)
 
-## 1. Function to process the uploaded image with user-defined parameters
-def process_image(input_image, bilateral_strength, clahe_clip_limit, clahe_tiles, 
+# Process the uploaded image with user-defined parameters
+def process_image(input_image, bilateral_strength, clahe_clip_limit, clahe_tiles,
                   sigma_sharpness, contrast, brightness,
                   sharpening_kernel_size, saturation_factor,bg_threshold, fg_threshold,
-                  erode_size, alpha_matting, mask, post_process, background_color):
+                  erode_size, alpha_matting, mask, post_process, background_color,mask_process= False):
     
     # Convert PIL Image to NumPy array
     input_image_np = np.array(input_image)
@@ -70,6 +76,9 @@ def process_image(input_image, bilateral_strength, clahe_clip_limit, clahe_tiles
     # Check if the image is grayscale and convert it to 3 channels if necessary
     if len(input_image_np.shape) == 2:
         input_image_np = cv2.cvtColor(input_image_np, cv2.COLOR_GRAY2BGR)      
+    
+    # Noise Reduction
+    #input_image_np = cv2.GaussianBlur(input_image_np, (5, 5), 0)
         
     # Split PIL image into its individual color channels
     blue, green, red = cv2.split(input_image_np)
@@ -106,14 +115,11 @@ def process_image(input_image, bilateral_strength, clahe_clip_limit, clahe_tiles
 
     # Apply sharpening kernel to image using filter2D
     processed_image = cv2.filter2D(result_image, -1, kernel)
-    
-    # --------- added -------
-    # contrast and brightness adjustment
-    # alpha controls contrast and beta controls brightness
+        
+    # Alpha controls contrast and beta controls brightness
     processed_image = cv2.convertScaleAbs(processed_image, alpha= contrast, beta=brightness)
     
-    # Additional sharpening    
-    # Create the sharpening kernel and apply it to the image
+    # Additional sharpening: Create the sharpening kernel and apply it to the image
     custom_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
     
     # Sharpen the image
@@ -123,39 +129,49 @@ def process_image(input_image, bilateral_strength, clahe_clip_limit, clahe_tiles
     processed_image = cv2.GaussianBlur(processed_image, (0, 0), sigma_sharpness)
     
     # Finally, remove background
-    processed_image = rembg.remove(processed_image,
-                                   alpha_matting = alpha_matting,
-                                   alpha_matting_foreground_threshold=fg_threshold,
-                                   alpha_matting_background_threshold=bg_threshold,
-                                   alpha_matting_erode_size = erode_size,
-                                   only_mask = mask,
-                                   post_process_mask = post_process,
-                                   bgcolor = (0, 0, 0, 0) if background_color == "Transparent" else \
-                                             (255, 255, 255, 255) if background_color == "White" else \
-                                             (0, 0, 0, 255)
-                                )
-    # --------- added
+    if mask_process == True:
+        processed_image = rembg.remove(processed_image,
+                                    alpha_matting = alpha_matting,
+                                    alpha_matting_foreground_threshold=fg_threshold,
+                                    alpha_matting_background_threshold=bg_threshold,
+                                    alpha_matting_erode_size = erode_size,
+                                    only_mask = True,
+                                    post_process_mask = True,
+                                    bgcolor = (0, 0, 0, 0) if background_color == "Transparent" else \
+                                                (255, 255, 255, 255) if background_color == "White" else \
+                                                (0, 0, 0, 255)
+                                    )
+    else:
+        processed_image = rembg.remove(processed_image,
+                                    alpha_matting = alpha_matting,
+                                    alpha_matting_foreground_threshold=fg_threshold,
+                                    alpha_matting_background_threshold=bg_threshold,
+                                    alpha_matting_erode_size = erode_size,
+                                    only_mask = mask,
+                                    post_process_mask = post_process,
+                                    bgcolor = (0, 0, 0, 0) if background_color == "Transparent" else \
+                                                (255, 255, 255, 255) if background_color == "White" else \
+                                                (0, 0, 0, 255)
+        )                    
+      
     return Image.fromarray(processed_image)
 
-## 2. Main function to run the streamlit app
+# Run the streamlit app
 def main():
-    # Set title for the App
-    #st.markdown("# :orange[Image preprocessing]")
-    st.markdown('<div style="text-align: center;"><h1 style="color: orange;">Image preprocessing</h1></div>', unsafe_allow_html=True)
-    #st.markdown("## For optimal background removal from an image")   
-    st.markdown('<div style="text-align: center;"><h2 style="font-size: 26px;">For optimal background removal from an image</h2></div>', unsafe_allow_html=True)
-        
+    st.markdown('<div style="text-align: center;"><h1 style="color: orange;">Image preprocessing</h1></div>',
+                unsafe_allow_html=True)
+    st.markdown('<div style="text-align: center;"><h2 style="font-size: 26px;">For optimal background removal from '
+                'an image</h2></div>', unsafe_allow_html=True)
+
     # Draw a dividing line
-    #st.divider()
+    st.markdown("---")
 
     # Step 1: upload image file as jpg/jpeg, include label
     uploaded_file = st.sidebar.file_uploader(" #### :camera: :orange[1. Upload Image] ", type=["JPG", "JPEG"])
 
     if uploaded_file is not None:        
         
-        # Step 2: Slider customization with live updates
-        
-        # First block with sliders
+        # Step 2: Slider customization with live updates       
         with st.expander("",expanded=True):
             # Set subtitle and short explanation
             st.write("#### :orange[2a. Apply Image Filters]")
@@ -165,13 +181,13 @@ def main():
             # create 2 columns to distribute sliders
             col1, col2 = st.columns(2)
 
-            # Column 1: create sliders for Bilateral Filter and Saturation
+            # Column 1
             bilateral_strength = col1.slider("###### :heavy_check_mark: :blue[Bilateral Filter Strength] (preset = 5)", min_value=0, max_value=15, value=5, key='bilateral')
             saturation_factor =  col1.slider("###### :heavy_check_mark: :blue[Color Saturation] (preset = 1.1)", min_value=0.0, max_value=2.0, step=0.05, value=1.1, key='saturation')
             clahe_clip_limit =   col1.slider("###### :heavy_check_mark: :blue[CLAHE Clip Limit - Threshold for contrast limiting] (preset = 2)", min_value=0.0, max_value=5.0, value=2.0, step=0.05, key='clahe')
-            clahe_tiles =        col1.slider("###### :heavy_check_mark: :blue[CLAHE Tile Grid Size - Tile size for local contrast enhancement] (preset = 8,8)", min_value=2, max_value=15, value=8, step= 1, key='tiles')            
-                        
-            # Column 2: create sliders for CLAHE and Sharpening            
+            clahe_tiles =        col1.slider("###### :heavy_check_mark: :blue[CLAHE Tile Grid Size - Tile size for local contrast enhancement] (preset = 8,8)", min_value=2, max_value=15, value=8, step= 1, key='tiles')
+            
+            # Column 2
             sigma_sharpness = col2.slider("###### :heavy_check_mark: :blue[Sharpness Sigma] (preset = 0.5)", min_value=0.0, max_value=3.0, value=0.5, step=0.1, key='sharpness')
             contrast = col2.slider("###### :heavy_check_mark: :blue[Contrast] (preset = 1.0)", min_value=0.1, max_value=3.0, value=1.0, step=0.1, key='contrast')
             brightness = col2.slider("###### :heavy_check_mark: :blue[Brightness] (preset = 10)", min_value=-100, max_value=100, value=10, step=1, key='brightness')
@@ -189,34 +205,43 @@ def main():
             col3, col4 = st.columns(2)
         
             # Draw a dividing line
-            #st.divider()
+            st.markdown("---")
             
             # mask
             alpha_matting = col3.checkbox("Use Alpha matting", value = False) # Alpha matting is a post processing step that can be used to improve the quality of the output.
             mask = col3.checkbox("Keep mask only", value = False)
             post_process = col3.checkbox("Postprocess mask", value = False)   # You can use the post_process_mask argument to post process the mask to get better results.       
             background_color = col3.radio("Background Color", ["Transparent", "White", "Black"])
-
             
             bg_threshold = col4.slider('###### :heavy_check_mark: :blue[Background Threshold] (default = 10)', min_value= 0, max_value = 255, value = 10, key = 'background') # [default: 10]
             fg_threshold = col4.slider('###### :heavy_check_mark: :blue[Foreground Threshold] (default = 200)', min_value = 0,max_value = 255, value = 200, key = 'foreground') # [default: 240]
             erode_size   = col4.slider('###### :heavy_check_mark: :blue[Erode Size] (default = 10)', min_value = 0,max_value = 25, value = 10, key = 'erode') # [default: 10]
                         
         
-        # Step 3: Live Preview of Image Processing
+        # Step 3: Live preview of image processing
         if uploaded_file is not None:
             
             col1, col2 = st.columns(2)
-            # Read uploaded image
+            
             image = Image.open(uploaded_file)
             
             # Display uploaded image with label
             col1.image(image, caption="Uploaded Image", use_column_width=True)
             
             # Process image with updated parameters
-            processed_image_pil = process_image(image, bilateral_strength, clahe_clip_limit,clahe_tiles,
-                                                sigma_sharpness,contrast, brightness ,sharpening_kernel_size, saturation_factor,
-                                                bg_threshold, fg_threshold, erode_size, alpha_matting, mask, post_process, background_color)
+            processed_image_pil = process_image(image, bilateral_strength, clahe_clip_limit,
+                                                                     clahe_tiles, sigma_sharpness, contrast,
+                                                                     brightness, sharpening_kernel_size,
+                                                                     saturation_factor, bg_threshold, fg_threshold,
+                                                                     erode_size, alpha_matting, mask, post_process, background_color,mask_process= False)
+            
+            # Process mask with updated parameters
+            processed_mask_pil = process_image(image, bilateral_strength, clahe_clip_limit,
+                                                                     clahe_tiles, sigma_sharpness, contrast,
+                                                                     brightness, sharpening_kernel_size,
+                                                                     saturation_factor, bg_threshold, fg_threshold,
+                                                                     erode_size, alpha_matting, mask, post_process, background_color,mask_process= True)
+
 
             # Display resulting image dynamically
             col2.image(processed_image_pil, caption="Processed Image", use_column_width=True)
@@ -227,22 +252,33 @@ def main():
             # Construct file names for processed images
             processed_image_name_jpeg = f"{original_name}_processed.jpg"
             processed_image_name_png = f"{original_name}_processed.png"
+            extracted_image_name_jpeg = f"{original_name}_extracted.jpg"
+            extracted_image_name_png = f"{original_name}_extracted.png"
             
-            # Convert processed image to RGB (removing alpha channel)
-            processed_image_rgb = processed_image_pil.convert("RGB")
-
-            # Save processed image in JPEG format in-memory
+            # Save processed image in JPEG format
             jpeg_buffer = BytesIO()
-            processed_image_rgb.save(jpeg_buffer, format="JPEG")
+            processed_image_pil.convert("RGB").save(jpeg_buffer, format="JPEG")
             jpeg_data = jpeg_buffer.getvalue()
 
-            # Save processed image in PNG format in-memory using imageio
+            # Save processed image in PNG format
             png_buffer = BytesIO()
             imageio.imwrite(png_buffer, np.array(processed_image_pil), format='PNG')
             png_data = png_buffer.getvalue()
-            
-            # Draw a dividing line
-            st.divider()
+
+            # Apply mask to original image
+            extracted_image_pil = apply_mask_and_convert_to_pil(np.array(image), np.array(processed_mask_pil))
+
+            # Save extracted image in JPEG format
+            extracted_jpeg_buffer = BytesIO()
+            extracted_image_pil.save(extracted_jpeg_buffer, format="JPEG")
+            extracted_jpeg_data = extracted_jpeg_buffer.getvalue()
+
+            # Save extracted image in PNG format
+            extracted_png_buffer = BytesIO()
+            imageio.imwrite(extracted_png_buffer, np.array(extracted_image_pil), format='PNG')
+            extracted_png_data = extracted_png_buffer.getvalue()
+
+            st.markdown("---")
             
             # Step 4: Download Buttons
             # write subtitle and download information
@@ -250,30 +286,40 @@ def main():
             st.sidebar.markdown(f"#### :floppy_disk: :orange[3. Download Processed Image] ")
             st.sidebar.write('Download image in compressed JPG or losless PNG file format:')
             
-            # Provide download buttons for both formats
-            # create 2 columns to distribute download buttons
-            #col1, col2 = st.columns(2)
-            # place download button for jpeg file in column 1
-            #with col1:
-            st.sidebar.download_button(
+            # Provide download buttons for both formats            
+            col1, col2 = st.columns(2)
+            
+            # Place download button for jpeg file in column 1
+            with col1:
+                st.sidebar.download_button(
                     label=f":orange[JPG] ({processed_image_name_jpeg})",
                     data=jpeg_data,
                     file_name=processed_image_name_jpeg,
                     key="processed_image_download_jpeg",
-            )            
-                        
-            # place download button for png file in column 2
-            #with col2:
-            st.sidebar.download_button(
+                )
+                st.sidebar.download_button(
+                    label=f":orange[JPG - Extracted] ({extracted_image_name_jpeg})",
+                    data=extracted_jpeg_data,
+                    file_name=extracted_image_name_jpeg,
+                    key="extracted_image_download_jpeg",
+                )
+            
+            # Place download button for png file in column 2
+            with col2:
+                st.sidebar.download_button(
                     label=f":orange[PNG] ({processed_image_name_png})",
                     data=png_data,
                     file_name=processed_image_name_png,
                     key="processed_image_download_png",
-            )
+                )
+                st.sidebar.download_button(
+                    label=f":orange[PNG - Extracted] ({extracted_image_name_png})",
+                    data=extracted_png_data,
+                    file_name=extracted_image_name_png,
+                    key="extracted_image_download_png",
+                )
 
-            # Download message
-            #st.caption("Preparing Download... Please wait.")
-  
 # run main function
 if __name__ == "__main__":
     main()
+    
