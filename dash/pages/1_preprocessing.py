5f445c4811bb1a3350ae33c7c376ed2724945ba1
Stef Smeets <s.smeets@esciencecenter.nl>
2024-05-01 15:37:19 +0200

    Refactor dashboard into widgets

--- dash/pages/1_preprocessing.py
+++ dash/pages/1_preprocessing.py
@@ -1,108 +1,134 @@
 from __future__ import annotations
 
-from io import BytesIO
 import imageio.v3 as imageio
+import numpy as np
 import streamlit as st
 from styling import set_custom_css
 from unraphael.preprocess import apply_mask, process_image, remove_background
+from pathlib import Path
+
 
 _process_image = st.cache_data(process_image)
 _apply_mask = st.cache_data(process_image)
 _remove_background = st.cache_data(remove_background)
 
 
-def main():
-    set_custom_css()
-
-    st.sidebar.title('Image preprocessing')
-
+def load_image():
+    load_example = st.sidebar.checkbox('Load example', value=False)
     uploaded_file = st.sidebar.file_uploader('Upload Image ', type=['JPG', 'JPEG'])
 
-    if not uploaded_file:
-        st.info('Upload file to continue...')
-        st.stop()
+    if load_example:
+        image_drc = Path('../data/raw/Bridgewater')
+        image_file = image_drc / '0_Edinburgh_Nat_Gallery.jpg'
+    else:
+        if not uploaded_file:
+            st.info('Upload image to continue')
+            st.stop()
 
-    st.subheader('Apply Image Filters')
-    with st.expander('Click to expand...', expanded=False):
+        image_file = uploaded_file
+
+    name, _ = image_file.name.rsplit('.')
+    image = imageio.imread(image_file)
+
+    return name, image
+
+
+def preprocess_image_widget(image: np.ndarray):
+    st.subheader('Preprocessing')
+    with st.expander('Click to expand...', expanded=True):
         st.write(
             'The processed image is shown with a preset of parameters. Use the sliders to explore the effects of image filters, or to'
             'refine the adjustment. When you are happy with the result, download the processed image.'
         )
 
-        col1, col2 = st.columns(2)
+        col1, col2, col3, col4 = st.columns(4)
 
         image_params = {}
 
         image_params['bilateral_strength'] = col1.slider(
-            'Bilateral Filter Strength (preset = 5)',
+            'Bilateral Filter Strength',
             min_value=0,
             max_value=15,
             value=5,
             key='bilateral',
+            help='(default = 5)',
         )
         image_params['saturation_factor'] = col1.slider(
-            'Color Saturation (preset = 1.1)',
+            'Color Saturation',
             min_value=0.0,
             max_value=2.0,
             step=0.05,
             value=1.1,
             key='saturation',
+            help='(default = 1.1)',
         )
         image_params['clahe_clip_limit'] = col1.slider(
-            'CLAHE Clip Limit - Threshold for contrast limiting (preset = 2)',
+            'CLAHE Clip Limit',
             min_value=0.0,
             max_value=5.0,
             value=2.0,
             step=0.05,
             key='clahe',
+            help='Threshold for contrast limiting (default = 2)',
         )
         image_params['clahe_tiles'] = col1.slider(
-            'CLAHE Tile Grid Size - Tile size for local contrast enhancement (preset = 8,8)',
+            'CLAHE Tile Grid Size',
             min_value=2,
             max_value=15,
             value=8,
             step=1,
             key='tiles',
+            help='Tile size for local contrast enhancement (default = 8)',
         )
 
         image_params['sigma_sharpness'] = col2.slider(
-            'Sharpness Sigma (preset = 0.5)',
+            'Sharpness Sigma',
             min_value=0.0,
             max_value=3.0,
             value=0.5,
             step=0.1,
             key='sharpness',
+            help='(default = 0.5)',
         )
         image_params['contrast'] = col2.slider(
-            'Contrast (preset = 1.0)',
+            'Contrast',
             min_value=0.1,
             max_value=3.0,
             value=1.0,
             step=0.1,
             key='contrast',
+            help='(default = 1.0)',
         )
         image_params['brightness'] = col2.slider(
-            'Brightness (preset = 10)',
+            'Brightness',
             min_value=-100,
             max_value=100,
             value=10,
             step=1,
             key='brightness',
+            help='(default = 10)',
         )
         image_params['sharpening_kernel_size'] = col2.slider(
-            'Sharpening Kernel Size (preset = 3)',
+            'Sharpening kernel size',
             min_value=1,
             max_value=9,
             step=2,
             value=3,
             key='sharpen',
+            help='(default = 3)',
         )
+    out = process_image(image, **image_params)
+    col3.image(image, 'before')
+    col4.image(out, 'after')
+    return out
 
-    st.subheader('Parameters for background removal')
-    with st.expander('Click to expand...', expanded=False):
+
+def remove_background_widget(image: np.ndarray) -> np.ndarray:
+    st.subheader('Background removal')
+    with st.expander('Click to expand...', expanded=True):
         st.write('Change these parameters to tune the background removal.')
 
-        col1, col2 = st.columns(2)
+        col1, col2, col3, col4 = st.columns(4)
 
         background_params = {}
 
@@ -123,83 +149,83 @@ def main():
         }
 
         background_params['bgcolor'] = col1.radio(
-            'Background Color',
+            'Background color',
             bgmap.keys(),
             format_func=lambda x: bgmap[x],
             help='You can use the post_process_mask argument to post process the mask to get better results.',
         )
 
         background_params['bg_threshold'] = col2.slider(
-            'Background Threshold (default = 10)',
+            'Background threshold',
             min_value=0,
             max_value=255,
             value=10,
             key='background',
+            help='(default = 10)',
         )
         background_params['fg_threshold'] = col2.slider(
-            'Foreground Threshold (default = 200)',
+            'Foreground threshold',
             min_value=0,
             max_value=255,
             value=200,
             key='foreground',
+            help='(default = 200)',
         )
         background_params['erode_size'] = col2.slider(
-            'Erode Size (default = 10)',
+            'Erode size',
             min_value=0,
             max_value=25,
             value=10,
             key='erode',
+            help='(default = 10)',
         )
 
-    image = imageio.imread(uploaded_file)
+    nobg = remove_background(image, **background_params, mask_process=False)
 
-    processed_image = process_image(image, **image_params)
+    mask = remove_background(image, **background_params, mask_process=True)
 
-    processed_nobg = remove_background(
-        processed_image, **background_params, mask_process=False
-    )
-    processed_mask = remove_background(
-        processed_image, **background_params, mask_process=True
-    )
+    col3.image(mask, 'mask')
+    col4.image(nobg, 'background removed')
 
-    col1, col2 = st.columns(2)
-    col1.image(image, caption='Uploaded Image', use_column_width=True)
-    col2.image(processed_nobg, caption='Processed Image', use_column_width=True)
-
-    png_buffer = BytesIO()
-    imageio.imwrite(png_buffer, processed_image, format='PNG')
-
-    extracted_image = apply_mask(image, processed_mask)
-
-    extracted_png_buffer = BytesIO()
-    imageio.imwrite(extracted_png_buffer, extracted_image, format='PNG')
-
-    st.markdown('---')
-
-    st.subheader('Download Processed Image')
-
-    col1, col2 = st.columns(2)
-
-    stem, _ = uploaded_file.name.rsplit('.')
-    processed_image_name_png = f'{stem}_processed.png'
-
-    col1.download_button(
-        label=f'PNG ({processed_image_name_png})',
-        data=png_buffer.getvalue(),
-        file_name=processed_image_name_png,
-        key='processed_image_download_png',
-    )
-
-    extracted_image_name_png = f'{stem}_extracted.png'
-
-    col2.download_button(
-        label=f'PNG - Extracted ({extracted_image_name_png})',
-        data=extracted_png_buffer.getvalue(),
-        file_name=extracted_image_name_png,
-        key='extracted_image_download_png',
-    )
+    return nobg, mask
 
 
-# run main function
+def image_downloads(*, basename: str, images: dict[str, np.ndarray]):
+    st.subheader('Download Images')
+
+    cols = st.columns(len(images))
+
+    for col, key in zip(cols, images):
+        image = images[key]
+
+        col.image(image, caption=key.upper(), use_column_width=True)
+
+        filename = f'{basename}_{key}.png'
+
+        col.download_button(
+            label=f'Download ({filename})',
+            data=imageio.imwrite('<bytes>', image, extension='.png'),
+            file_name=filename,
+            key=filename,
+        )
+
+
+def main():
+    set_custom_css()
+
+    name, image = load_image()
+
+    processed = preprocess_image_widget(image)
+    processed_nobg, processed_mask = remove_background_widget(processed)
+
+    images = {
+        'original': image,
+        'processed': processed_nobg,
+        'extracted': apply_mask(image, processed_mask),
+    }
+
+    image_downloads(basename=name, images=images)
+
+
 if __name__ == '__main__':
     main()
