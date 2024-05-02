3cb3827d3ff9f39e2ff29ad8e9cf7de61cef10a9
Stef Smeets <s.smeets@esciencecenter.nl>
2024-05-02 10:37:05 +0200

    Add upload for config

--- dash/widgets.py
+++ dash/widgets.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from pathlib import Path
 from typing import TYPE_CHECKING
-
+import imageio.v3 as imageio
 import streamlit as st
 from config import (
     _load_config,
@@ -17,13 +17,15 @@ from unraphael.feature import (
     heatmap_to_condensed_distance_matrix,
 )
 from unraphael.io import load_images_from_drc, load_images
-
-_load_images_from_drc = st.cache_data(load_images_from_drc)
-_load_images = st.cache_data(load_images)
-
+from importlib.resources import files
 
 if TYPE_CHECKING:
     import numpy as np
+
+data_directory = files('unraphael.data')
+
+_load_images = st.cache_data(load_images)
+_load_images_from_drc = st.cache_data(load_images_from_drc)
 
 
 def show_images_widget(images: dict[str, np.ndarray], *, n_cols: int = 4):
@@ -43,10 +45,34 @@ def show_images_widget(images: dict[str, np.ndarray], *, n_cols: int = 4):
     return selected
 
 
+def load_image() -> tuple[str, np.ndarray]:
+    """Widget to load a single image with default."""
+    load_example = st.sidebar.checkbox('Load example', value=False)
+    uploaded_file = st.sidebar.file_uploader('Upload Image ', type=['JPG', 'JPEG'])
+
+    if load_example:
+        image_drc = Path('../data/raw/Bridgewater')
+        image_file = image_drc / '0_Edinburgh_Nat_Gallery.jpg'
+    else:
+        if not uploaded_file:
+            st.info('Upload image to continue')
+            st.stop()
+
+        image_file = uploaded_file
+
+    name, _ = image_file.name.rsplit('.')
+    image = imageio.imread(image_file)
+
+    return name, image
+
+
 def load_config_widget():
     """Widget to load config file."""
-    config_fn = st.text_input(label='Config file', value='config.yaml')
-    config = _load_config(config_fn)
+    uploaded_file = st.sidebar.file_uploader('Load config ', type=['yml', 'yaml'])
+    if not uploaded_file:
+        uploaded_file = data_directory / 'config.yaml'
+
+    config = _load_config(uploaded_file)
     if 'config' not in st.session_state:
         _update_session_state(config)
 
