b67c131e1e355fc523b51cf5634dfc2c0f640870
Stef Smeets <s.smeets@esciencecenter.nl>
2024-05-14 10:30:05 +0200

    Remove smoke test
    For some reason parametrize tests fails on CI

--- tests/dash_test.py
+++ tests/dash_test.py
@@ -1,12 +1,9 @@
 from streamlit.testing.v1 import AppTest
 from pathlib import Path
-import pytest
 import sys
 
 dashdir = Path(__file__).parents[1] / 'dash'
 sys.path.append(str(dashdir))
-
-pages = list((dashdir / 'pages').glob('*.py'))
 
 
 def test_home():
@@ -15,28 +12,10 @@ def test_home():
     assert not at.exception
 
 
-@pytest.mark.parametrize('page', pages)
-def test_pages(page):
-    at = AppTest.from_file(str(page))
-    at.run()
-    assert not at.exception
-
-
-def test_page1():
-    at = AppTest.from_file(str(dashdir / 'pages' / '1_preprocess.py'))
-    at.run()
-    assert not at.exception
-
-
-def test_page2():
-    at = AppTest.from_file(str(dashdir / 'pages' / '2_image_similarity.py'))
-    at.run()
-    assert not at.exception
-
-
 def test_preprocess():
     at = AppTest.from_file(str(dashdir / 'pages' / '1_preprocess.py'))
     at.run()
+    assert not at.exception
 
     assert 'load_example' in at.session_state
     at.session_state['load_example'] = True
@@ -46,7 +25,9 @@ def test_preprocess():
 
 
 def test_image_similarity():
-    at = AppTest.from_file(str(dashdir / 'pages' / '2_image_similarity.py')).run()
+    at = AppTest.from_file(str(dashdir / 'pages' / '2_image_similarity.py'))
+    at.run()
+    assert not at.exception
 
     assert 'continue_ransac' not in at.session_state
     assert 'load_example' in at.session_state
