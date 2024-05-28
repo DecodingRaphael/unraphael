db9a100fed307c2eb22089d7257d23ace047ad96
Stef Smeets <s.smeets@esciencecenter.nl>
2024-05-28 17:02:20 +0200

    Refactor skeleton drawer

--- src/unraphael/dash/pages/3_detect_objects.py
+++ src/unraphael/dash/pages/3_detect_objects.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from pathlib import Path
 from ultralytics import YOLO
 import streamlit as st
@@ -6,6 +8,8 @@ import cv2
 from widgets import image_downloads_widget, load_image_widget
 from styling import set_custom_css
 import platformdirs
+from dataclasses import dataclass
+
 
 CACHEDIR = Path(platformdirs.user_cache_dir('unraphael'))
 
@@ -62,6 +66,90 @@ def segmentation_task(*, results):
     return objects
 
 
+@dataclass
+class BodyDrawer:
+    keypoints: np.ndarray
+    HEAD: tuple[int, int, int] = (0, 255, 0)  # green
+    BODY: tuple[int, int, int] = (0, 0, 255)  # blue
+    LEGS: tuple[int, int, int] = (255, 165, 0)  # orange
+    REST: tuple[int, int, int] = (128, 0, 128)  # purple
+
+    def draw(self, *, image):
+        self.draw_points_head(image=image)
+        self.draw_points_legs(image=image)
+        self.draw_points_body(image=image)
+        self.draw_skeleton_legs(image=image)
+        self.draw_skeleton_body(image=image)
+        self.draw_skeleton_head(image=image)
+        self.draw_additional(image=image)
+
+    def _draw_points(self, *, image, indices, color):
+        for j, kp in enumerate(self.keypoints):
+            if j in indices:
+                if kp[2] > 0.5:
+                    x, y = (
+                        int(kp[0]),
+                        int(kp[1]),
+                    )
+                    cv2.circle(image, (x, y), 5, color, -1)
+
+    def draw_points_head(self, *, image):
+        # indices for eyes, ears, and nose
+        head_indices = [0, 1, 2, 3, 4]
+        self._draw_points(image=image, indices=head_indices, color=self.HEAD)
+
+    def draw_points_body(self, *, image):
+        # indices for shoulders, elbows, and wrists
+        body_indices = [5, 6, 7, 8, 9, 10]
+        self._draw_points(image=image, indices=body_indices, color=self.BODY)
+
+    def draw_points_legs(self, *, image):
+        # indices for hips, knees, and ankles
+        leg_indices = [11, 13, 15, 12, 14, 16]
+        self._draw_points(image=image, indices=leg_indices, color=self.LEGS)
+
+    def _draw_skeleton(self, *, image, indices, color):
+        for j1, j2 in indices:
+            kp1 = self.keypoints[j1]
+            kp2 = self.keypoints[j2]
+
+            # check confidence of both keypoints
+            if kp1[2] > 0.5 and kp2[2] > 0.5:
+                p1 = (int(kp1[0]), int(kp1[1]))
+                p2 = (int(kp2[0]), int(kp2[1]))
+                cv2.line(image, p1, p2, color, 2)
+
+    def draw_skeleton_legs(self, *, image):
+        # left hip to left knee, left knee to left ankle
+        # right hip to right knee, right knee to right ankle
+        indices = [(11, 13), (13, 15), (12, 14), (14, 16)]
+        self._draw_skeleton(image=image, indices=indices, color=self.LEGS)
+
+    def draw_skeleton_body(self, *, image):
+        # shoulder to shoulder, shoulder to elbow, elbow to wrist
+        indices = [(5, 6), (5, 7), (7, 9), (6, 8), (8, 10)]
+        self._draw_skeleton(image=image, indices=indices, color=self.BODY)
+
+    def draw_skeleton_head(self, *, image):
+        # nose to eyes, eyes to ears
+        indices = [(0, 1), (1, 2), (0, 3), (3, 4), (0, 2), (2, 4)]
+        self._draw_skeleton(image=image, indices=indices, color=self.HEAD)
+
+    def draw_additional(self, *, image):
+        # left ear to left shoulder, right ear to right shoulder
+        self._draw_skeleton(image=image, indices=[(3, 5)], color=self.HEAD)
+        # right ear to right shoulder, left ear to left shoulder
+        self._draw_skeleton(image=image, indices=[(4, 6)], color=self.HEAD)
+        # left shoulder to left hip
+        self._draw_skeleton(image=image, indices=[(5, 11)], color=self.REST)
+        # right shoulder to right hip
+        self._draw_skeleton(image=image, indices=[(6, 12)], color=self.REST)
+        # left shoulder to right shoulder
+        self._draw_skeleton(image=image, indices=[(5, 6)], color=self.REST)
+        # left hip to right hip
+        self._draw_skeleton(image=image, indices=[(11, 12)], color=self.REST)
+
+
 def pose_task(*, results):
     objects = {}
 
@@ -73,112 +161,7 @@ def pose_task(*, results):
             black_image = np.zeros((frame_height, frame_width, 3), dtype=np.uint8)
             keypoints = keypoints.cpu().numpy()
 
-            # Draw keypoints for legs (orange)
-            for j in range(len(keypoints)):
-                if j in [11, 13, 15, 12, 14, 16]:
-                    # indices for hips, knees, and ankles
-                    if keypoints[j][2] > 0.5:
-                        x, y = (
-                            int(keypoints[j][0]),
-                            int(keypoints[j][1]),
-                        )
-                        cv2.circle(black_image, (x, y), 5, (255, 165, 0), -1)
-
-            # Draw keypoints for body (blue)
-            for j in range(len(keypoints)):
-                if j in [5, 6, 7, 8, 9, 10]:
-                    # indices for shoulders, elbows, and wrists
-                    if keypoints[j][2] > 0.5:
-                        x, y = (
-                            int(keypoints[j][0]),
-                            int(keypoints[j][1]),
-                        )
-                        cv2.circle(black_image, (x, y), 5, (0, 0, 255), -1)
-
-            # Draw keypoints for head (green)
-            for j in range(len(keypoints)):
-                if j in [0, 1, 2, 3, 4]:
-                    # indices for eyes, ears, and nose
-                    if keypoints[j][2] > 0.5:
-                        x, y = (
-                            int(keypoints[j][0]),
-                            int(keypoints[j][1]),
-                        )
-                        cv2.circle(black_image, (x, y), 5, (0, 255, 0), -1)
-
-            # Define the skeleton (pairs of keypoints to connect) according to COCO dataset order
-            # Left Hip to Left Knee, Left Knee to Left Ankle
-            # Right Hip to Right Knee, Right Knee to Right Ankle
-            skeleton_legs = [(11, 13), (13, 15), (12, 14), (14, 16)]
-            # Shoulder to Shoulder, Shoulder to Elbow, Elbow to Wrist
-            skeleton_body = [(5, 6), (5, 7), (7, 9), (6, 8), (8, 10)]
-            # Nose to Eyes, Eyes to Ears
-            skeleton_head = [(0, 1), (1, 2), (0, 3), (3, 4), (0, 2), (2, 4)]
-
-            # Define additional skeleton connections
-            additional_connections = [
-                (3, 5),
-                (4, 6),
-                (5, 6),  # left shoulder to right shoulder
-                (11, 12),  # left hip to right hip
-                (5, 11),  # Left Shoulder to Left Hip
-                (6, 12),  # Right Shoulder to Right Hip
-            ]
-
-            # Draw skeleton for legs (orange)
-            for j1, j2 in skeleton_legs:
-                # check confidence of both keypoints
-                if keypoints[j1][2] > 0.5 and keypoints[j2][2] > 0.5:
-                    p1 = (int(keypoints[j1][0]), int(keypoints[j1][1]))
-                    p2 = (int(keypoints[j2][0]), int(keypoints[j2][1]))
-                    # orange
-                    cv2.line(black_image, p1, p2, (255, 165, 0), 2)
-
-            # Draw skeleton for body
-            for j1, j2 in skeleton_body:
-                # check confidence of both keypoints
-                if keypoints[j1][2] > 0.5 and keypoints[j2][2] > 0.5:
-                    p1 = (int(keypoints[j1][0]), int(keypoints[j1][1]))
-                    p2 = (int(keypoints[j2][0]), int(keypoints[j2][1]))
-                    # blue
-                    cv2.line(black_image, p1, p2, (0, 0, 255), 2)
-
-            # Draw skeleton for head (green)
-            for j1, j2 in skeleton_head:
-                # check confidence of both keypoints
-                if keypoints[j1][2] > 0.5 and keypoints[j2][2] > 0.5:
-                    p1 = (int(keypoints[j1][0]), int(keypoints[j1][1]))
-                    p2 = (int(keypoints[j2][0]), int(keypoints[j2][1]))
-                    # green
-                    cv2.line(black_image, p1, p2, (0, 255, 0), 2)
-
-            # Draw additional skeleton connections
-            for j1, j2 in additional_connections:
-                # check confidence of both keypoints
-                if keypoints[j1][2] > 0.5 and keypoints[j2][2] > 0.5:
-                    p1 = (int(keypoints[j1][0]), int(keypoints[j1][1]))
-                    p2 = (int(keypoints[j2][0]), int(keypoints[j2][1]))
-
-                    if j1 in [3, 5] and j2 in [3, 5]:
-                        # left ear to left shoulder, right ear to right shoulder
-                        # green
-                        cv2.line(black_image, p1, p2, (0, 255, 0), 2)
-                    elif j1 in [4, 6] and j2 in [4, 6]:
-                        # right ear to right shoulder, left ear to left shoulder
-                        # green
-                        cv2.line(black_image, p1, p2, (0, 255, 0), 2)
-                    elif j1 in [0, 5, 11] and j2 in [0, 5, 11]:
-                        # shoulders and hips
-                        cv2.line(black_image, p1, p2, (128, 0, 128), 2)
-                    elif j1 in [0, 6, 12] and j2 in [0, 6, 12]:
-                        # shoulders and hips
-                        cv2.line(black_image, p1, p2, (128, 0, 128), 2)
-                    elif j1 in [5, 6] and j2 in [5, 6]:
-                        # left and right shoulders
-                        cv2.line(black_image, p1, p2, (0, 0, 255), 2)
-                    elif j1 in [11, 12] and j2 in [11, 12]:
-                        # left and right hips
-                        cv2.line(black_image, p1, p2, (128, 0, 128), 2)
+            BodyDrawer(keypoints).draw(image=black_image)
 
             # Save the black image with keypoints to dictionary
             objects[f'pose_{idx}_{ci}'] = black_image
