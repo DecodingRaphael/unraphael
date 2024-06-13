5cd0b4369884f1b06bbef808ee5ccbc9eda9da64
Stef Smeets <s.smeets@esciencecenter.nl>
2024-06-13 13:27:47 +0200

    Refactor image alignment/equalization code

--- src/unraphael/dash/pages/4_normalization.py
+++ src/unraphael/dash/pages/4_normalization.py
@@ -11,18 +11,15 @@ from styling import set_custom_css
 from widgets import load_images_widget, show_images_widget
 
 
-def image_alignment_widget(*, base_image: np.ndarray, images: dict[str, np.ndarray]):
-    """This widget helps to align all images to the given base image."""
-    col1, col2 = st.columns(2)
+def equalize_images_widget(*, base_image: np.ndarray, images: dict[str, np.ndarray]):
+    """This widget helps with equalizing images."""
+    st.subheader('Equalization parameters')
 
-    with col1:
-        st.subheader('Equalization parameters')
-
-        brightness = st.checkbox('Equalize brightness', value=False)
-        contrast = st.checkbox('Equalize contrast', value=False)
-        sharpness = st.checkbox('Equalize sharpness', value=False)
-        color = st.checkbox('Equalize colors', value=False)
-        reinhard = st.checkbox('Reinhard color transfer', value=False)
+    brightness = st.checkbox('Equalize brightness', value=False)
+    contrast = st.checkbox('Equalize contrast', value=False)
+    sharpness = st.checkbox('Equalize sharpness', value=False)
+    color = st.checkbox('Equalize colors', value=False)
+    reinhard = st.checkbox('Reinhard color transfer', value=False)
 
     preprocess_options = {
         'brightness': brightness,
@@ -32,65 +29,15 @@ def image_alignment_widget(*, base_image: np.ndarray, images: dict[str, np.ndarr
         'reinhard': reinhard,
     }
 
-    with st.expander('Help for parameters for aligning images', expanded=False):
-        st.write(
-            (
-                'The following methods are used for image registration and alignment. '
-                'Depending on your specific alignment requirements and computational '
-                'constraints, you may choose one method over the other. Example usage '
-                'scenarios and comparative analysis can help you choose the most suitable '
-                'alignment technique for your specific requirements.'
-            )
-        )
-        st.write(
-            (
-                '- **Feature-based Alignment (ORB, SIFT or SURF)**: '
-                'Utilizes feature detection and matching for estimating '
-                'translation, rotation, shear, and scaling. Suitable for images with '
-                'distinct features and complex transformations. Note that keypoint '
-                'matching may fail with poor feature detection.'
-            )
-        )
-        st.write(
-            (
-                '- **Enhanced Correlation Coefficient (ECC) Maximization**: '
-                'Identifies the geometric transformation that maximizes the correlation '
-                'coefficient between two images. It can handle translation, rotation, and '
-                'scaling, especially accurate for small to moderate transformations, '
-                'and robust to noise and varying illumination.'
-            )
-        )
-        st.write(
-            (
-                '- **Fast Fourier Transform (FFT) Phase Correlation Method**: '
-                'Primarily designed for translational shifts. For rotation, consider '
-                'alternate methods like log-polar transform or feature matching. Efficient '
-                'for translational alignment but may not handle rotation or '
-                'scaling effectively.'
-            )
-        )
-        st.write(
-            (
-                '- **Fourier Mellin Transform (FMT) Method**: Logarithm of the Fourier '
-                'magnitude of an image followed by another Fourier transform to obtain a '
-                'log-polar transform. Rotation and scale invariant but computationally '
-                'intensive compared to other methods.'
-            )
-        )
-        st.write(
-            (
-                '- **Rotation Alignment Method**: Aligns images by finding the '
-                'optimal rotation to minimize the difference between them. Suited when '
-                'rotation is the primary misalignment source and computational cost '
-                'is not a major concern.'
-            )
-        )
-        st.write(
-            (
-                '- **User-provided keypoints** (from pose estimation): '
-                'Aligns images based on user-provided keypoints obtained from pose estimation.'
-            )
-        )
+    return {
+        name: equalize_images(base_image, image, **preprocess_options)
+        for name, image in images.items()
+    }
+
+
+def align_images_widget(*, base_image: np.ndarray, images: dict[str, np.ndarray]):
+    """This widget helps with aligning images."""
+    st.subheader('Alignment parameters')
 
     options = [
         'Feature based alignment',
@@ -100,65 +47,55 @@ def image_alignment_widget(*, base_image: np.ndarray, images: dict[str, np.ndarr
         'Rotational Alignment',
         'User-provided keypoints (from pose estimation)',
     ]
-    with col2:
-        st.subheader('Alignment parameters')
 
-        selected_option = st.selectbox(
-            'Alignment procedure:',
-            options,
+    selected_option = st.selectbox(
+        'Alignment procedure:',
+        options,
+        help=(
+            '**Feature based alignment**: Aligns images based on detected features using '
+            'algorithms like SIFT, SURF, or ORB.'
+            '\n\n**Enhanced Correlation Coefficient Maximization**: Estimates the '
+            'he parameters of a geometric transformation between two images by '
+            'maximizing the correlation coefficient.'
+            '\n\n**Fourier Mellin Transform**: Uses the Fourier Mellin Transform to align '
+            'images based on their frequency content.'
+            '\n\n**FFT phase correlation**: Aligns images by computing '
+            'the phase correlation between their Fourier transforms.'
+            '\n\n**Rotational Alignment**: Aligns images by rotating them to a common '
+            'orientation.'
+        ),
+    )
+
+    if selected_option == 'Feature based alignment':
+        motion_model = st.selectbox(
+            'Algorithm:',
+            ['SIFT', 'SURF', 'ORB'],
+        )
+    elif selected_option == 'Enhanced Correlation Coefficient Maximization':
+        motion_model = st.selectbox(
+            'Motion model:',
+            ['translation', 'euclidian', 'affine', 'homography'],
             help=(
-                '**Feature based alignment**: Aligns images based on detected features using '
-                'algorithms like SIFT, SURF, or ORB.'
-                '\n\n**Enhanced Correlation Coefficient Maximization**: Estimates the '
-                'he parameters of a geometric transformation between two images by '
-                'maximizing the correlation coefficient.'
-                '\n\n**Fourier Mellin Transform**: Uses the Fourier Mellin Transform to align '
-                'images based on their frequency content.'
-                '\n\n**FFT phase correlation**: Aligns images by computing '
-                'the phase correlation between their Fourier transforms.'
-                '\n\n**Rotational Alignment**: Aligns images by rotating them to a common '
-                'orientation.'
+                'The motion model defines the transformation between the base '
+                'image and the input images. Translation is the simplest model, '
+                'while homography is the most complex.'
             ),
         )
-
-        if selected_option == 'Feature based alignment':
-            motion_model = st.selectbox(
-                'Algorithm:',
-                ['SIFT', 'SURF', 'ORB'],
-            )
-        elif selected_option == 'Enhanced Correlation Coefficient Maximization':
-            motion_model = st.selectbox(
-                'Motion model:',
-                ['translation', 'euclidian', 'affine', 'homography'],
-                help=(
-                    'The motion model defines the transformation between the base '
-                    'image and the input images. Translation is the simplest model, '
-                    'while homography is the most complex.'
-                ),
-            )
-        elif selected_option == 'Fourier Mellin Transform':
-            motion_model = st.selectbox(
-                'Normalization method for cross correlation',
-                [None, 'normalize', 'phase'],
-                help=(
-                    'The normalization applied in the cross correlation. If `None`, '
-                    'the cross correlation is not normalized. If `normalize`, the '
-                    'cross correlation is normalized by the product of the magnitudes of the '
-                    'Fourier transforms of the images. If `phase`, the cross '
-                    'correlation is normalized by the product of the magnitudes and phases '
-                    'of the Fourier transforms of the images.'
-                ),
-            )
-        else:
-            motion_model = None
-
-    # base_image = images[base_name]
-    # other_images = {name: image for name, image in images.items() if name != base_name}
-
-    images = {
-        name: equalize_images(base_image, image, **preprocess_options)
-        for name, image in images.items()
-    }
+    elif selected_option == 'Fourier Mellin Transform':
+        motion_model = st.selectbox(
+            'Normalization method for cross correlation',
+            [None, 'normalize', 'phase'],
+            help=(
+                'The normalization applied in the cross correlation. If `None`, '
+                'the cross correlation is not normalized. If `normalize`, the '
+                'cross correlation is normalized by the product of the magnitudes of the '
+                'Fourier transforms of the images. If `phase`, the cross '
+                'correlation is normalized by the product of the magnitudes and phases '
+                'of the Fourier transforms of the images.'
+            ),
+        )
+    else:
+        motion_model = None
 
     return align_all_selected_images_to_template(
         base_image=base_image,
@@ -166,6 +103,83 @@ def image_alignment_widget(*, base_image: np.ndarray, images: dict[str, np.ndarr
         selected_option=selected_option,
         motion_model=motion_model,
     )
+
+
+def alignment_help_widget():
+    st.write(
+        (
+            'The following methods are used for image registration and alignment. '
+            'Depending on your specific alignment requirements and computational '
+            'constraints, you may choose one method over the other. Example usage '
+            'scenarios and comparative analysis can help you choose the most suitable '
+            'alignment technique for your specific requirements.'
+        )
+    )
+    st.write(
+        (
+            '- **Feature-based Alignment (ORB, SIFT or SURF)**: '
+            'Utilizes feature detection and matching for estimating '
+            'translation, rotation, shear, and scaling. Suitable for images with '
+            'distinct features and complex transformations. Note that keypoint '
+            'matching may fail with poor feature detection.'
+        )
+    )
+    st.write(
+        (
+            '- **Enhanced Correlation Coefficient (ECC) Maximization**: '
+            'Identifies the geometric transformation that maximizes the correlation '
+            'coefficient between two images. It can handle translation, rotation, and '
+            'scaling, especially accurate for small to moderate transformations, '
+            'and robust to noise and varying illumination.'
+        )
+    )
+    st.write(
+        (
+            '- **Fast Fourier Transform (FFT) Phase Correlation Method**: '
+            'Primarily designed for translational shifts. For rotation, consider '
+            'alternate methods like log-polar transform or feature matching. Efficient '
+            'for translational alignment but may not handle rotation or '
+            'scaling effectively.'
+        )
+    )
+    st.write(
+        (
+            '- **Fourier Mellin Transform (FMT) Method**: Logarithm of the Fourier '
+            'magnitude of an image followed by another Fourier transform to obtain a '
+            'log-polar transform. Rotation and scale invariant but computationally '
+            'intensive compared to other methods.'
+        )
+    )
+    st.write(
+        (
+            '- **Rotation Alignment Method**: Aligns images by finding the '
+            'optimal rotation to minimize the difference between them. Suited when '
+            'rotation is the primary misalignment source and computational cost '
+            'is not a major concern.'
+        )
+    )
+    st.write(
+        (
+            '- **User-provided keypoints** (from pose estimation): '
+            'Aligns images based on user-provided keypoints obtained from pose estimation.'
+        )
+    )
+
+
+def image_alignment_widget(*, base_image: np.ndarray, images: dict[str, np.ndarray]):
+    """This widget helps to align all images to the given base image."""
+    col1, col2 = st.columns(2)
+
+    with col1:
+        images = equalize_images_widget(base_image=base_image, images=images)
+
+    with col2:
+        images = align_images_widget(base_image=base_image, images=images)
+
+    with st.expander('Help for parameters for aligning images', expanded=False):
+        alignment_help_widget()
+
+    return images
 
 
 def comparison_widget(
