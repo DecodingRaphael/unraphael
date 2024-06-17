a3aff07eb9d7d6f7303f27137ae6f04150a70a42
Stef Smeets <s.smeets@esciencecenter.nl>
2024-06-17 09:24:57 +0200

    Remove xfail marker

--- tests/dash_test.py
+++ tests/dash_test.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import sys
 
-import pytest
 from streamlit.testing.v1 import AppTest
 
 from unraphael.locations import dash_directory
@@ -16,11 +15,6 @@ def test_home():
     assert not at.exception
 
 
-@pytest.mark.xfail(
-    reason='Fails with Thread "MainThread": missing ScriptRunContext'
-    'on the CI because of rembg dependency. Only the first time the page is'
-    'loaded does the test fail.'
-)
 def test_preprocess_load():
     at = AppTest.from_file(str(dash_directory / 'pages' / '1_preprocess.py'))
     at.run(timeout=5)
